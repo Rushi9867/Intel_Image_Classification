8c6e96f17104a725e021312d03caca17b33d18ab
Rushikesh <rushikhandare2203@gmail.com>
2024-07-06 16:36:17 +0530

    Template added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Intel-Image-Classification-with-PyTorch',
+    version='0.0.1',
+    author='Rushikesh Khandare',
+    author_email='rushikhandare2203@gmail.com',
+    install_requires=[],
+    packages=find_packages()
+)
